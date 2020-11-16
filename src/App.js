1569d4ba1b2bac5f3367bc0e4e5ab0d2970e722b
TigranVardanyan <tig.vardanyan.001@gmail.com>
2020-11-16 22:43:57 +0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//class App extends Component {
+//  constructor() {
+//    super();
+//    this.state = {
+//      sayHello: "Hello",
+//    };
+//    console.log("[Constructor]")
+//  }
+//  changeHello(){
+//    this.setState({sayHello:"Hello Tigran"})
+//  }
+//
+//  static getDerivedStateFromProps(props, state) {
+//    console.log("[static getDerivedStateFromProps()]");
+//    return null;
+//  };
+//  componentDidiMount() {
+//    console.log("[componentDidiMount]")
+//  }
+//
+//  //shouldComponentUpdate() {
+//  //  console.log("[shouldComponentUpdate]");
+//  //  return false;
+//  //};
+//
+//
+//
+//  render() {
+//    console.log("[render()]");
+//    return (
+//      <div>
+//        Hello React
+//        <p>{this.state.sayHello}</p>
+//        <button
+//          id={"btn"}
+//          onClick={this.changeHello}
+//          >
+//          Button
+//        </button>
+//      </div>
+//    )
+//  }
+//
+//
+//}
+
+class App extends React.Component {
+  constructor(props) {
+    console.log("[Constructor]");
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    console.log("[componentDidMount]");
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+    console.groupEnd()
+  }
+
+  shouldComponentUpdate() {
+    console.log("[shouldComponentUpdate()]");
+    if (Date.now() % 2 == 0) {
+      return true;
+    } else {
+      console.groupEnd()
+      return false
+    }
+  }
+
+  componentDidUpdate() {
+    console.log("[componentDidUpdate()]");
+    console.groupEnd()
+  }
+
+  componentWillUnmount() {
+    console.groupEnd()
+    console.group("Lifecycle end");
+    clearInterval(this.timerID);
+    console.log("[componentWillUnmount()]");
+    console.groupEnd()
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  static getDerivedStateFromProps() {
+    console.group('Lifecycle');
+    console.log("[static getDerivedStateFromProps()]");
+    return null
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("[getSnapshotBeforeUpdate()]");
+    return null
+  }
+
+
+  render() {
+    console.log("[render()]");
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={() => this.componentWillUnmount()}>Unmount</button>
+      </div>
+    );
+  }
 }
 
 export default App;
